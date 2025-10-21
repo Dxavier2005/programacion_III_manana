661e5350c67696cf9151fe7e9c2fdfa9ecd6e6aa
Dxavier2005 <dariosimbanasalcan@gmail.com>
2025-10-21 10:02:41 -0500

    21-10-2025

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,33 @@
+for (let i = 1; i <10; i++) {
+    console.log("numero", i);
+}
+
+suma=0;
+for (let i = 1; i <=10; i++) {
+    suma += i;
+}
+console.log("sumatorias indices", suma);
+let nombre="Francisco";
+for (let i = 0; i <= nombre.length; i++) {
+    console.log(nombre[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tabla de multiplicar del 4
+
+
+console.log("=== TABLA DE MULTIPLICAR DEL 4 ===");
+for (let i = 1; i <= 10; i++) {
+    console.log(`4 x ${i} = ${4 * i}`);
+}
